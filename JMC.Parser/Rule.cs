93c773411e9e8471957a57310f3d8bff82afd05a
bdke <91001643+bdke@users.noreply.github.com>
2024-10-15 15:45:44 +0800

    Add rule builderi

---
+++ JMC.Parser/Rule.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Immutable;
+
+namespace JMC.Parser;
+public sealed class Rule
+{
+    private readonly List<BaseRule> rules = [];
+
+    private ImmutableArray<BaseRule>? builtArray;
+
+    public ImmutableArray<BaseRule> Rules => builtArray ?? throw new InvalidOperationException("The rule is not built");
+
+    public Rule Add(params BaseRule[] subRules)
+    {
+        rules.AddRange(subRules);
+        return this;
+    }
+
+    public void Build()
+    {
+        builtArray = [.. rules];
+    }
+}
---
+++ JMC.Parser/TokenType.cs
@@ -0,0 +1,17 @@
+﻿namespace JMC.Parser;
+public enum TokenType : int
+{
+    SimpleAddictive,
+    Multipative,
+    Addictive,
+    Comparator,
+    Comparasion,
+    Assign,
+    AssignOperators,
+    Arrow,
+    BlockStart,
+    BlockEnd,
+    ListStart,
+    ListEnd,
+
+}
