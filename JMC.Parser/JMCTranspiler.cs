fa0c0b62b029327485c829f02459fb4075ece393
bdke <91001643+bdke@users.noreply.github.com>
2024-11-11 16:54:51 +0800

    Add transpiler base

---
+++ JMC.Parser/JMCTranspiler.cs
@@ -0,0 +1,10 @@
+﻿namespace JMC.Parser;
+public sealed class JMCTranspiler(IEnumerable<JMCExpression> roots, IEnumerable<JMCFileDetail> fileDetails)
+{
+    private readonly ReadOnlyMemory<JMCExpression> _roots = new(roots.ToArray());
+    private readonly ReadOnlyMemory<JMCFileDetail> _fileDetails = new(fileDetails.ToArray());
+
+    public ReadOnlySpan<JMCFileDetail> FileDetails => _fileDetails.Span;
+    public ReadOnlySpan<JMCExpression> Roots => _roots.Span;
+
+}
